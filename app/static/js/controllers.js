af7beefb6f772f002bac43761946434fcfc26595
Makoto Kadowaki <makoto@venidera.com>
2015-08-21 02:20:49 -0300

    Add more AngularStrap demos

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -28,7 +28,7 @@ angular.module('lion.controllers', [])
 }])
 
 // Select Controller
-.controller('SelectDemoCtrl', function($scope, $http) {
+.controller('SelectDemoCtrl',  ['$scope', '$http', function($scope, $http) {
   $scope.selectedIcon = '';
   $scope.selectedIcons = ['Globe', 'Heart'];
   $scope.icons = [
@@ -39,10 +39,10 @@ angular.module('lion.controllers', [])
   ];
   $scope.selectedMonth = 0;
   $scope.months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-})
+}])
 
 // Dropdown Controller
-.controller('DropdownDemoCtrl', function($scope, $alert) {
+.controller('DropdownDemoCtrl',  ['$scope', '$alert', function($scope, $alert) {
   $scope.dropdown = [
     {text: '<i class="fa fa-download"></i>&nbsp;Another action', href: '#anotherAction'},
     {text: '<i class="fa fa-globe"></i>&nbsp;Display an alert', click: '$alert("Holy guacamole!")'},
@@ -53,5 +53,37 @@ angular.module('lion.controllers', [])
   $scope.$alert = function(title) {
     $alert({title: title, content: 'Best check yo self, you\'re not looking too good.', placement: 'top', type: 'info', keyboard: true, show: true});
   };
-})
+}])
+
+// Buttons Controller
+.controller('ButtonDemoCtrl',  ['$scope', function($scope) {
+  $scope.button = {
+    toggle: false,
+    checkbox: {left: false, middle: true, right: false},
+    radio: 'left'
+  };
+}])
+
+
+// Typeahead Controller
+.controller('TypeaheadDemoCtrl', ['$scope', '$templateCache', '$http', function($scope, $templateCache, $http) {
+  $scope.selectedState = '';
+  $scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+  $scope.selectedIcon = '';
+  $scope.icons = [
+    {value: 'Gear', label: '<i class="fa fa-gear"></i> Gear'},
+    {value: 'Globe', label: '<i class="fa fa-globe"></i> Globe'},
+    {value: 'Heart', label: '<i class="fa fa-heart"></i> Heart'},
+    {value: 'Camera', label: '<i class="fa fa-camera"></i> Camera'}
+  ];
+
+  $scope.selectedAddress = '';
+  $scope.getAddress = function(viewValue) {
+    var params = {address: viewValue, sensor: false};
+    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {params: params})
+    .then(function(res) {
+      return res.data.results;
+    });
+  };
+}])
 ;
