8f764c143ba98ed71b5b7bc256413a0b19b555e9
Makoto Kadowaki <makoto@venidera.com>
2015-10-10 00:14:25 -0300

    Ajustes no aside_menu. Adicionando handler para as rotas. Adicionando templates e definindo rotas

--- app/handlers/main.py
+++ app/handlers/main.py
@@ -6,3 +6,25 @@ from handlers.base import BaseHandler
 class MainHandler(BaseHandler):
     def get(self):
         self.render('main.html')
+
+class SideMenuHandler(BaseHandler):
+    def get(self):
+        self.render('side_menu.html')
+
+class NewLionHandler(BaseHandler):
+    def get(self):
+        self.render('new_lion.html')
+
+class SearchLionHandler(BaseHandler):
+    def get(self):
+        self.render('search_lion.html')
+
+class NewImageSetHandler(BaseHandler):
+    def get(self):
+        self.render('new_image_set.html')
+
+class SearchImageSetHandler(BaseHandler):
+    def get(self):
+        self.render('search_image_set.html')
+
+
