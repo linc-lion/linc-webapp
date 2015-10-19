9888a1eff5073e10e8758b6affe86ed7a7799077
Makoto Kadowaki <makoto@venidera.com>
2015-10-19 20:38:36 -0200

    separando controller do home

--- app/static/js/controllers/controllers.js
+++ app/static/js/controllers/controllers.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('lion.guardians.controllers', ['lion.guardians.login.controllers',
+                                                               'lion.guardians.home.controllers',
                                                                'lion.guardians.lions.controllers',
                                                                'lion.guardians.image.set.controllers',
                                                                'lion.guardians.conservationists.controllers'])
