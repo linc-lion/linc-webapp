f416304beef12cbf19c4cb754ba93e90227a9420
Andre Toscano <andre@venidera.com>
2015-11-06 02:32:56 -0200

    solving the problem for cvrequest creation - it was the content type

--- app/settings.py
+++ app/settings.py
@@ -100,3 +100,5 @@ config['url'] = appurl
 
 # Settings for access to linc-api
 config['API_URL'] = 'https://linc-api.herokuapp.com'
+#Dev url 'http://192.168.100.10:5000'
+#Deploy test url 'https://linc-api.herokuapp.com'
