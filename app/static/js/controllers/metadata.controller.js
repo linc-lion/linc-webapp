e6ce4cf3c320ed746926ab380d5eacddd1d3355d
Makoto Kadowaki <makoto@venidera.com>
2015-12-02 01:17:19 -0200

    Add Unknown to gender. put Notes here in placeholder

--- app/static/js/controllers/metadata.controller.js
+++ app/static/js/controllers/metadata.controller.js
@@ -289,7 +289,7 @@ angular.module('lion.guardians.metadata.controller', ['lion.guardians.metadata.d
   }
 
   // Gender List
-  $scope.genders = [{value: 'male', label: 'Male'}, {value: 'female',label: 'Female'}];
+  $scope.genders = [{value: 'male', label: 'Male'}, {value: 'female',label: 'Female'}, {value: 'unknown',label: 'Unknown'}];
   // Markings List
   $scope.markings = [
     {value: 'ear',  label: 'Ear', allText : 'All Ear Markings',
@@ -378,7 +378,7 @@ angular.module('lion.guardians.metadata.controller', ['lion.guardians.metadata.d
                         "latitude":"", "longitude": "", "gender": "",
                         "markings": {'ear': [],'mount': [],'tail': []},
                         "broken_teeth": [], "eye_damage": [],
-                        "nose_color": undefined, "scars": [], "notes": "Notes here"
+                        "nose_color": undefined, "scars": [], "notes": ""
     };
     $scope.tooltip = {'show_lion_name': true};
   }
