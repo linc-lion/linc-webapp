edba12745b18b9611f11a78a866b887a4a1f137e
Andre E Toscano <andre@MacBook-Pro-de-Andre.local>
2015-12-10 00:25:13 -0200

    added conservationists page

--- app/static/js/controllers/conservationists.controller.js
+++ app/static/js/controllers/conservationists.controller.js
@@ -2,6 +2,20 @@
 
 angular.module('lion.guardians.conservationists.controller', [])
 
-.controller('ConservationistsCtrl', ['$scope', function ($scope) {
+.controller('ConservationistsCtrl', ['$scope','$state','$http','$cookies', function ($scope,$state,$http,$cookies) {
 
+  $scope.orgs = '';
+  var xsrfcookie = $cookies.get('_xsrf');
+  var req = { method: 'GET',
+                 url: '/users/conservationists',
+                data: {},
+             headers: { 'Content-Type': 'application/json', 'X-XSRFToken' : xsrfcookie},
+              config: {}};
+  $http(req).then(function(results){
+    console.log(results.data.data);
+    $scope.orgs = results.data.data;
+   }, function(){
+    $state.go("home");
+  });
+  //$scope.orgs = data;
 }]);
