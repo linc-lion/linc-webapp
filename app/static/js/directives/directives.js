95bc4066aa3bc741b0167307981b4be3f6f63ab5
Makoto Kadowaki <makoto@venidera.com>
2016-11-23 13:01:59 -0200

    Updating admin imageset

--- app/static/js/directives/directives.js
+++ app/static/js/directives/directives.js
@@ -123,17 +123,6 @@ angular.module('linc.directive', [])
   };
 })
 
-/*.directive('watchScope', [function () {
-  return {
-    scope: {
-      item: '=watchScope'
-    },
-    link: function (scope, element, attrs) {
-      console.log('element ' + scope.item.name + ' created');
-    }
-  };
-}])*/
-
 .directive('repeatDone', function() {
   return function(scope, element, attrs) {
     if (scope.$last) { // all are rendered
@@ -142,45 +131,21 @@ angular.module('linc.directive', [])
   }
 })
 
-/*.directive('uiSrefActiveIf', ['$state', function($state) {
-  return {
-    restrict: "A",
-    controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
-      var state = $attrs.uiSrefActiveIf;
-      function update() {
-        if ( $state.includes(state) || $state.is(state) ) {
-          $element.addClass("active");
-        } else {
-          $element.removeClass("active");
-        }
-      }
-      $scope.$on('$stateChangeSuccess', update);
-      update();
-    }]
-  };
-}])*/
-
-/**
- * Provides with pagination with infinite scroll to handle large list of choices. 
- * An upfront large list of choices makes the control unstable and unresponsive.
- * This feature avoid populaing the list upfront by pagination which is the primary cause of unstability.
- * Pagination works in 2 scenarios:-
- * 
- * 1) Simple scrolling of the contents.
- * 2) Scrolling when the Autocomplete/search text is enteded and the results are still too large.
- * 
- * @example
-    <ui-select-choices position="up" all-choices="ctrl.allTenThousandItems"  refresh-delay="0"
-        repeat="person in $select.pageOptions.people | propsFilter: {name: $select.search, age: $select.search} ">
-      <div ng-bind-html="person.name | highlight: $select.search"></div>
-      <small>
-        email: {{person.email}}
-        age: <span ng-bind-html="''+person.age | highlight: $select.search"></span>
-      </small>
-    </ui-select-choices>
- * 
- * 
- */
+//Provides with pagination with infinite scroll to handle large list of choices. 
+//An upfront large list of choices makes the control unstable and unresponsive.
+//This feature avoid populaing the list upfront by pagination which is the primary cause of unstability.
+//Pagination works in 2 scenarios:- 
+// 1) Simple scrolling of the contents.
+// 2) Scrolling when the Autocomplete/search text is enteded and the results are still too large.
+// @example
+// <ui-select-choices position="up" all-choices="ctrl.allTenThousandItems"  refresh-delay="0"
+//     repeat="person in $select.pageOptions.people | propsFilter: {name: $select.search, age: $select.search} ">
+//   <div ng-bind-html="person.name | highlight: $select.search"></div>
+//   <small>
+//     email: {{person.email}}
+//     age: <span ng-bind-html="''+person.age | highlight: $select.search"></span>
+//   </small>
+// </ui-select-choices>
  .directive('uiSelectChoices', ['$timeout', '$parse', '$compile', '$document', '$filter', 
   function($timeout, $parse, $compile, $document, $filter) {
   return function(scope, elm, attr) {
@@ -212,7 +177,6 @@ angular.module('linc.directive', [])
               $event: event
             });
             scrollCompleted = true;
-
           };
           $timeout(callback, 100);
         }
@@ -226,7 +190,6 @@ angular.module('linc.directive', [])
     });
 
     scope.addMoreItems = function(doneCalBack) {
-      console.log('new addMoreItems');
       var $select = scope.$select;
       var allItems = scope.pagingOptions.allOptions;
       var moreItems = [];
@@ -238,29 +201,20 @@ angular.module('linc.directive', [])
         pageSize: 20,
         items: $select.items
       };
-
       if (pagingOptions.page === 0) {
         pagingOptions.items.length = 0;
       }
       if (!pagingOptions.originalAllItems) {
         pagingOptions.originalAllItems = scope.pagingOptions.allOptions;
       }
-      console.log('search term=' + search);
-      console.log('prev search term=' + pagingOptions.prevSearch);
       var searchDidNotChange = search && pagingOptions.prevSearch && search == pagingOptions.prevSearch;
-      console.log('isSearchChanged=' + searchDidNotChange);
       if (pagingOptions.filteredItems && searchDidNotChange) {
         allItems = pagingOptions.filteredItems;
       }
       pagingOptions.prevSearch = search;
       if (search && search.length > 0 && pagingOptions.items.length < allItems.length && !searchDidNotChange) {
-        //search
-
-
         if (!pagingOptions.filteredItems) {
-          //console.log('previous ' + pagingOptions.filteredItems);
         }
-
         pagingOptions.filteredItems = undefined;
         moreItems = $filter('filter')(pagingOptions.originalAllItems, search);
         //if filtered items are too many scrolling should occur for filtered items
@@ -268,9 +222,7 @@ angular.module('linc.directive', [])
           if (!pagingOptions.filteredItems) {
             pagingOptions.page = 0;
             pagingOptions.items.length = 0;
-          } else {
-
-          }
+          } else {}
           pagingOptions.page = 0;
           pagingOptions.items.length = 0;
           allItems = pagingOptions.filteredItems = moreItems;
@@ -280,20 +232,14 @@ angular.module('linc.directive', [])
           pagingOptions.items.length = 0;
           pagingOptions.filteredItems = undefined;
         }
-
-
-      } else {
-        console.log('plain paging');
-      }
+      } else {}
       pagingOptions.page++;
       if (pagingOptions.page * pagingOptions.pageSize < allItems.length) {
         moreItems = allItems.slice(pagingOptions.items.length, pagingOptions.page * pagingOptions.pageSize);
       }
-
       for (var k = 0; k < moreItems.length; k++) {
         pagingOptions.items.push(moreItems[k]);
       }
-
       scope.calculateDropdownPos();
       scope.$broadcast('uis:refresh');
       if (doneCalBack) doneCalBack();
