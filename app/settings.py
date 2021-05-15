cfd24e6cb8425c4948ed89b59fe48a31daac2a32
Makoto Kadowaki <makoto@venidera.com>
2021-05-14 23:51:01 -0300

    correcting API_URL

--- app/settings.py
+++ app/settings.py
@@ -87,6 +87,4 @@ config['APP_URL'] = environ.get('APP_URL', 'http://localhost:5080')
 config['login_url'] = config['APP_URL'] + '/#/login'
 
 # Setting linc-api URL
-# config['API_URL'] = environ.get('API_URL', 'http://localhost:5050')
-# config['API_URL'] = 'https://linc-api.herokuapp.com'
-config['API_URL'] = 'http://localhost:5050'
+config['API_URL'] = environ.get('API_URL', 'http://localhost:5050')
